cc52c12e66e7f81be19396b12ba7b944b94fd60a
nogsmujeeb <51092725+nogsmujeeb@users.noreply.github.com>
2021-12-07 17:10:32 +0530

    Update config.php

--- hms/include/config.php
+++ hms/include/config.php
@@ -7,6 +7,6 @@ $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
 {
- <!--echo "Failed to connect to MySQL: " . mysqli_connect_error();-->
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 ?>
